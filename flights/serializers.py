89ee439771ed07865153220cc31fdaf9fd45c64f
BibiTalal <bibi.t_alfahad@hotmail.com>
2022-11-02 03:55:59 +0300

    Register View Completed

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,7 +1,23 @@
+from dataclasses import field
+from pyexpat import model
 from rest_framework import serializers
-
 from flights.models import Booking, Flight
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True)
+    class Meta:
+        model=User
+        fields=["username","password","first_name","last_name"]
+    
+    def create(self, validated_data):
+        username=validated_data["username"]
+        password=validated_data["password"]
+        new_user=User(username=username)
+        new_user.set_password(password)
+        new_user.save()
+        return validated_data
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
--- flights/views.py
+++ flights/views.py
@@ -5,6 +5,9 @@ from rest_framework import generics
 from flights import serializers
 from flights.models import Booking, Flight
 
+class RegisterView(generics.CreateAPIView):
+    serializer_class=serializers.RegisterSerializer
+
 
 class FlightsList(generics.ListAPIView):
     queryset = Flight.objects.all()
